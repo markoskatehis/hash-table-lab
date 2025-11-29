6bd953e4fb2c08d64a47daef3e3389d61c361e62
Markos <markoskatehis@gmail.com>
2025-11-29 17:48:25 -0500

    Lab submission

--- ransom_note.py
+++ ransom_note.py
@@ -10,4 +10,15 @@ def can_construct(ransomNote: str, magazine: str) -> bool:
     Returns:
         bool: True if ransomNote can be constructed, False otherwise.
     """
-    pass  # TODO: Implement this function
+    char_counts = {}
+
+    for char in magazine:
+        char_counts[char] = char_counts.get(char, 0) + 1
+    
+    for char in ransomNote:
+        if char not in char_counts or char_counts[char] == 0:
+            return False
+        
+        char_counts[char] -= 1
+    
+    return True
